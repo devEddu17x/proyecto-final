bf1c4440f78d2b8d36f89aa780795657913b1515
devEddu <eduarjma@gmail.com>
2023-11-17 21:08:33 -0500

    add class Area, Puesto, Empleado

---
+++ src/main/java/entidades/Empleado.java
@@ -0,0 +1,12 @@
+package entidades;
+
+import java.time.LocalDate;
+
+public class Empleado {
+    private String codigo;
+    private String nombre;
+    private String apellido;
+    private String profesion;
+    private int dni;
+    private LocalDate inicioOrg;
+}
---
+++ src/main/java/entidades/Puesto.java
@@ -0,0 +1,10 @@
+package entidades;
+
+public class Puesto {
+    private String codigo;
+    private String nombre;
+    private String codigorArea;
+    private int maximoEmpleados;
+    private int numeroEmpleados;
+    
+}
